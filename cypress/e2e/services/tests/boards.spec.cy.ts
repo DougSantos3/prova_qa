cc1f8098f9d71cdb635f5dc20c82b1721c120f68
douglas <douglasmartinssantos7@gmail.com>
2024-08-01 20:56:21 -0300

    refactor

--- cypress/e2e/services/tests/boards.spec.cy.ts
+++ cypress/e2e/services/tests/boards.spec.cy.ts
@@ -7,10 +7,10 @@ const cookie = 'dsc=e77cc985d3aada2fa780f48e6f08aa0ce7d1b07247156d5538fdc77ea8f9
 
 it('Deve validar o contrato de uma board', () => {
   getBoards(cookie).then(response => {
-    expect(response.status).to.eq(200);
+    expect(response.status).to.eq(200)
 
     response.body.forEach(board => {
-      const { error } = boardsSchemaCompleted.validate(board);
+      const { error } = boardsSchemaCompleted.validate(board)
       expect(error).to.be.undefined;
     })
   })
@@ -19,7 +19,7 @@ it('Deve validar o contrato de uma board', () => {
 describe('Trello API Tests', () => {
   it('Deve obter a lista de boards do usuário', () => {
     getBoards(cookie).then(response => {
-      expect(response.status).to.eq(200);
+      expect(response.status).to.eq(200)
 
       response.body.forEach(board => {
         const { error } = boardsSchemaCompleted.validate(board)
@@ -32,22 +32,22 @@ describe('Trello API Tests', () => {
 describe('Trello API Tests', () => {
   it('Deve obter a lista de boards do usuário', () => {
     getBoards(cookie).then(response => {
-      expect(response.status).to.eq(200);
+      expect(response.status).to.eq(200)
 
       response.body.forEach(board => {
-        const { error } = boardSchemaWithoutClosed.validate(board);
-        expect(error).to.be.undefined;
-      });
-    });
-  });
+        const { error } = boardSchemaWithoutClosed.validate(board)
+        expect(error).to.be.undefined
+      })
+    })
+  })
 
   it('Deve validar o contrato de uma board', () => {
     getBoards(cookie).then(response => {
-      expect(response.status).to.eq(200);
+      expect(response.status).to.eq(200)
 
       response.body.forEach(board => {
-        const { error } = boardSchemaWithoutClosed.validate(board);
-        expect(error).to.be.undefined;
+        const { error } = boardSchemaWithoutClosed.validate(board)
+        expect(error).to.be.undefined
       })
     })
   })
