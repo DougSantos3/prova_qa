1467ca6481f6881d9dc8c96426e5a2247d217ea9
douglas <douglasmartinssantos7@gmail.com>
2024-08-06 11:39:06 -0300

    remove kanban

--- cypress/e2e/ui/board/pages/kanban.ts
+++
@@ -1,6 +0,0 @@
-export class KanbanPage {
-  
-  private board = 'id or ccs selector never xpath'
-  private card = 'id or ccs selector never xpath'
-  private optionCard = 'id or ccs selector never xpath'
-}
